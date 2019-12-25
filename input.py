e9f33bd75ae97c2d331195bc3e1b8a2bbe878e6e
Aman Sarosh <amansarosh13@gmail.com>
2019-12-25 14:00:24 -0800

    Created User Input

---
+++ input.py
@@ -0,0 +1,9 @@
+
+name = input("Enter your name: ")  # generates prompt; takes inputted user value and puts it into a variable called name
+age = input("Enter your age: ")
+print("Hello " + name + "! You are " + age)  # uses stored variables to show prompt
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)  # float added to take an number. decimal or whole
+print(result)  # show final input
