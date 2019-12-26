99982cf70262f3f91478daa98ce6abc1734e4a33
Aman Sarosh <amansarosh13@gmail.com>
2019-12-26 02:07:19 -0800

    Tuples Example with Prints

---
+++ tuples.py
@@ -0,0 +1,5 @@
+coordinates = (4, 5), (6, 7), (80, 84)  # tuples are like a coordinate. CANNOT BE CHANGED
+print(coordinates[0])  # in 4, 5. 4 starts at 0 and 5 starts at 1
+
+# lists use "[]" tuples use "()" lists can be changed; tuples cannot be.
+#  tuples are usually used for data that wont be changed.
