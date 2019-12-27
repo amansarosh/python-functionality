d97e8d4531ee2967d9907fe19397f3a3970ae67b
Aman Sarosh <amansarosh13@gmail.com>
2019-12-26 22:21:56 -0800

    Created Files to show understanding of Classes.

---
+++ objects_classes.py
@@ -0,0 +1,7 @@
+from student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 2.5, True)
+student3 = Student("Aman", "Computer Science", 4.0, False)
+
+print(student3.on_honor_roll())
