6a0c0316f4cfa054bbb3969a159e62e113715f0e
Aman Sarosh <amansarosh13@gmail.com>
2019-12-26 22:23:58 -0800

    Inputs and Loops Additions

--- input.py
+++ input.py
@@ -1,4 +1,4 @@
-
+'''
 name = input("Enter your name: ")  # generates prompt; takes inputted user value and puts it into a variable called name
 age = input("Enter your age: ")
 print("Hello " + name + "! You are " + age)  # uses stored variables to show prompt
@@ -7,3 +7,15 @@ num1 = input("Enter a number: ")
 num2 = input("Enter another number: ")
 result = float(num1) + float(num2)  # float added to take an number. decimal or whole
 print(result)  # show final input
+'''
+
+try:
+    print(10 / 0)  # can be removed to show second except
+    number = int(input(" Enter a number: "))
+    print(number)
+except ZeroDivisionError as err:
+    print(err)
+
+except ValueError:
+    print("That's not a number")
+
--- loops.py
+++ loops.py
@@ -10,4 +10,18 @@ elif not is_male and is_tall:
 else:
     print("You are either not mall or not tall or both")
 
+i = 1  # set defined value for i
+while i <= 10:  # will check if i is less than or equal to 10
+    print(i)  # will print i until condition is met
+    i += 1  # same as i = i + 1. This will keep adding 1 to i until i <=10
 
+print("Done With Loop")  # after all conditions are met then this will print
+
+# ------------------------------------------------------------------------------
+
+friends = ["Cody", "Allen", "Jack", "Quinn", "Zach"]
+for index in friends:  # to access "friend" you have to print the same name
+    print(index)  # needs to match
+
+for index in range(len(friends)):  # counts til 10
+    print(index)  # needs to match
