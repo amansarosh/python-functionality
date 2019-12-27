16ef24b45e88ef2ae90678bc507a6b9998bc0b3e
Aman Sarosh <amansarosh13@gmail.com>
2019-12-26 22:17:41 -0800

    Created Dictionary Show Month Conversion

---
+++ dictionaries.py
@@ -0,0 +1,24 @@
+# Dictionaries are always created using "{}"
+# Keys should always be unique.
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions.get("Dec", "Not a Valid Key"))
+
+
+
+
+
+
